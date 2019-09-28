772ac68465ce3d7dcf8714b756b6ee27e5716d6d
Wiseman S. Jeannot <wjeannot2013@fau.edu>
2019-09-28 15:44:03 -0400

    Added print more print out message

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,21 +1,18 @@
-def get_fib():
-
-    count = int(input("Enter a number: "))
+def fib():
+    count = int(input("Enter the number fib you want to print out : "))
+    print("You have enter: ", count)
     i = 1
     if count == 0:
-        fib =[]
+        fib = [0]
     elif count == 1:
-        fib =[1]
+        fib = [1]
     elif count == 2:
         fib = [1,1]
     elif count > 2:
         fib = [1,1]
-    while i < (count -1):
-        fib.append(fib[i] + fib[i-1])
-        i += 1
+        while i < count - 1:
 
-
-    print(fib)
-
-
-get_fib()
+            fib.append((fib[i] + fib[i-1]))
+            i += 1
+    return fib
+print("your fibonacci number is:", fib())
